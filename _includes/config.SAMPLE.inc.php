402c97a7627bb16b9176d82577bd3ca5091e4987
Greg Chetcuti <greg@chetcuti.com>
2013-03-19 03:05:23 -0700

    Update file identifier in _includes/config.SAMPLE.inc.php

--- _includes/config.SAMPLE.inc.php
+++ _includes/config.SAMPLE.inc.php
@@ -1,5 +1,5 @@
 <?php
-// config.inc.php
+// config.SAMPLE.inc.php
 // 
 // Domain Manager - A web-based application written in PHP & MySQL used to manage a collection of domain names.
 // Copyright (C) 2010 Greg Chetcuti
