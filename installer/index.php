0b3f7c5257424585b3bab1fbf71875fdb7d340cf
Greg Chetcuti <greg@chetcuti.com>
2013-03-24 16:20:15 -0700

    Add settings table creation to installer

--- installer/index.php
+++ installer/index.php
@@ -363,6 +363,17 @@ CREATE TABLE IF NOT EXISTS `segments` (
 ";
 $result = mysql_query($sql,$connection) or die(mysql_error());
 
+$sql = "
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int(10) NOT NULL auto_increment,
+  `db_version` float NOT NULL,
+  `insert_time` datetime NOT NULL,
+  `update_time` datetime NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+";
+$result = mysql_query($sql,$connection) or die(mysql_error());
+
 header("Location: ../_includes/system/test-data-generate.php");
 exit;
 ?>
