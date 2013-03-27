64d3e5385ab7c096c55fdc2ef17e88cfa0a6c813
Greg Chetcuti <greg@chetcuti.com>
2013-03-27 02:30:00 -0700

    Comment out lines saying to delete /installer/

--- _includes/system/installation-check.inc.php
+++ _includes/system/installation-check.inc.php
@@ -40,8 +40,8 @@ if (is_dir($full_install_path)) {
 		
 		$_SESSION['session_installation_mode'] = 0;
 
-			$_SESSION['session_result_message'] .= "<BR>The software has been installed, but the /installer/ directory still exists.<BR><BR>";
-			$_SESSION['session_result_message'] .= "You should delete the /installer/ directory from your web server.<BR><BR>";
+			// $_SESSION['session_result_message'] .= "<BR>The software has been installed, but the /installer/ directory still exists.<BR><BR>";
+			// $_SESSION['session_result_message'] .= "You should delete the /installer/ directory from your web server.<BR><BR>";
 		
 	}
 	
