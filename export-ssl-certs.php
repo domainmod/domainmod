80243d132643374f62acb2979da47345c142c314
Greg Chetcuti <greg@chetcuti.com>
2013-03-31 00:31:18 -0700

    Various UI tweaks

--- export-ssl-certs.php
+++ export-ssl-certs.php
@@ -137,7 +137,7 @@ exit;
 <title><?=$software_title?> :: <?=$page_title?></title>
 <?php include("_includes/head-tags.inc.php"); ?>
 </head>
-<body>
+<body onLoad="document.forms[0].elements[0].focus()";>
 <?php include("_includes/header.inc.php"); ?>
 <?php 
 $result = $result2;
