005b598be56b2094459c95df8b27d56f69822304
Greg Chetcuti <greg@chetcuti.com>
2013-04-06 05:13:36 -0700

    Removed TLD Breakdown from Control Panel menu

--- _includes/layout/menu-sub.inc.php
+++ _includes/layout/menu-sub.inc.php
@@ -54,14 +54,12 @@
 <?php } elseif ($software_section == "system") { ?>
 			<?php if ($_SESSION['session_is_admin'] == 1) { ?>
              &raquo; <a href="<?php if ($web_root != "/") echo $web_root; ?>/system/">Control Panel</a>
-             &nbsp;&nbsp;/&nbsp;&nbsp;<a href="<?php if ($web_root != "/") echo $web_root; ?>/system/tlds.php">TLD Breakdown</a>
              &nbsp;&nbsp;/&nbsp;&nbsp;<a href="<?php if ($web_root != "/") echo $web_root; ?>/system/update-profile.php">Update Profile</a>
              &nbsp;&nbsp;/&nbsp;&nbsp;<a href="<?php if ($web_root != "/") echo $web_root; ?>/system/change-password.php">Change Password</a>
              &nbsp;&nbsp;/&nbsp;&nbsp;<a href="<?php if ($web_root != "/") echo $web_root; ?>/system/admin/users.php">User List</a>
              &nbsp;&nbsp;/&nbsp;&nbsp;<a href="<?php if ($web_root != "/") echo $web_root; ?>/system/admin/add/user.php">Add New User</a>
 			<?php } else { ?>
              &raquo; <a href="<?php if ($web_root != "/") echo $web_root; ?>/system/">Control Panel</a>
-             &nbsp;&nbsp;/&nbsp;&nbsp;<a href="<?php if ($web_root != "/") echo $web_root; ?>/system/tlds.php">TLD Breakdown</a>
              &nbsp;&nbsp;/&nbsp;&nbsp;<a href="<?php if ($web_root != "/") echo $web_root; ?>/system/update-profile.php">Update Profile</a>
              &nbsp;&nbsp;/&nbsp;&nbsp;<a href="<?php if ($web_root != "/") echo $web_root; ?>/system/change-password.php">Change Password</a>
             <?php } ?>
