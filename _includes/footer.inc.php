44d7398b8237c43ff863bcd788a2605240981d71
Greg Chetcuti <greg@chetcuti.com>
2013-04-17 00:29:51 -0700

    Update footer text

--- _includes/footer.inc.php
+++ _includes/footer.inc.php
@@ -19,9 +19,9 @@
 	</div>
 
     <div class="footer-container">
-		<?=$software_title?> is free software created by <a target="_blank" class="subtlelink" href="http://chetcuti.com">Greg Chetcuti</a> and released under the GNU/GPL License.<br>
-        For more information on the GNU/GPL License <a target="_blank" class="subtlelink" href="http://www.gnu.org/licenses/">click here</a>.<br>
-        To obtain your own copy of the <?=$software_title?> software <a target="_blank" class="subtlelink" href="http://aysmedia.com/code/domain-manager/">click here</a>.<BR><BR>
+	    <?=$software_title?> is open source software released under the <a target="_blank" class="subtlelink" href="http://www.gnu.org/licenses/">GNU/GPL License</a><br>
+        To obtain your own copy of the <?=$software_title?> software <a target="_blank" class="subtlelink" href="http://aysmedia.com/code/domain-manager/">click here</a><BR>
+        Created by <a target="_blank" class="subtlelink" href="http://chetcuti.com">Greg Chetcuti</a> @ <a target="_blank" class="subtlelink" href="http://aysmedia.com">AYS Media</a><BR><BR>
         <a target="_blank" href="http://www.gnu.org/licenses/"><img border="0" width="88" height="31" src="https://www.gnu.org/graphics/gplv3-88x31.png" /></a>
     </div>
 
