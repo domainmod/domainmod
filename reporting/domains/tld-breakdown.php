5914c22ce2369e5b3a829288f5c8eb5ccbd1417d
Greg Chetcuti <greg@chetcuti.com>
2013-04-26 03:20:50 -0700

    Mov TLD breakdown file

--- reporting/domains/tld-breakdown.php
+++ reporting/domains/tld-breakdown.php
@@ -1,5 +1,5 @@
 <?php
-// /reporting/domain-tld-breakdown.php
+// /reporting/domains/tld-breakdown.php
 // 
 // Domain Manager - A web-based application written in PHP & MySQL used to manage a collection of domain names.
 // Copyright (C) 2010 Greg Chetcuti
@@ -16,24 +16,24 @@
 // see http://www.gnu.org/licenses/
 ?>
 <?php
-include("../_includes/start-session.inc.php");
-include("../_includes/config.inc.php");
-include("../_includes/database.inc.php");
-include("../_includes/software.inc.php");
-include("../_includes/auth/auth-check.inc.php");
+include("../../_includes/start-session.inc.php");
+include("../../_includes/config.inc.php");
+include("../../_includes/database.inc.php");
+include("../../_includes/software.inc.php");
+include("../../_includes/auth/auth-check.inc.php");
 
 $page_title = "Top Level Domain Breakdown";
 $software_section = "reporting";
 ?>
-<?php include("../_includes/doctype.inc.php"); ?>
+<?php include("../../_includes/doctype.inc.php"); ?>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title><?=$software_title?> :: <?=$page_title?></title>
-<?php include("../_includes/head-tags.inc.php"); ?>
+<?php include("../../_includes/head-tags.inc.php"); ?>
 </head>
 <body>
-<?php include("../_includes/header.inc.php"); ?>
+<?php include("../../_includes/header.inc.php"); ?>
 This is a breakdown of the Top Level Domains that are currently active in your <?=$software_title?>.
 <BR><BR>
 <?php
@@ -63,7 +63,7 @@ if (mysql_num_rows($result) > 0) { ?>
                 <?php echo ".$row->tld"; ?>
             </td>
             <td class="main_table_cell_active">
-                <a class="nobold" href="../domains.php?tld=<?=$row->tld?>"><?=number_format($row->total_tld_count)?></a>
+                <a class="nobold" href="../../domains.php?tld=<?=$row->tld?>"><?=number_format($row->total_tld_count)?></a>
             </td>
         </tr>
     <?php 
@@ -77,6 +77,6 @@ if (mysql_num_rows($result) > 0) { ?>
 
 }
 ?>
-<?php include("../_includes/footer.inc.php"); ?>
+<?php include("../../_includes/footer.inc.php"); ?>
 </body>
 </html>
