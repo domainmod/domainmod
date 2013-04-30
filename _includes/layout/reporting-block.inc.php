9c5c40f1e7ced429b58aae6e3563d6469c613446
Greg Chetcuti <greg@chetcuti.com>
2013-04-30 14:30:22 -0700

    Add SSL Cost by Category report

--- _includes/layout/reporting-block.inc.php
+++ _includes/layout/reporting-block.inc.php
@@ -42,6 +42,7 @@ Before running any reports you should <a href="<?=$web_root?>/_includes/system/u
         <form name="reporting_menu_domain_form" method="post" action="<?=$PHP_SELF?>">
             <select name="reporting_menu_ssl" onChange="MM_jumpMenu('parent',this,0)">
             <option value="<?=$web_root?>/reporting/"<?php if ($report_name == "") echo " selected" ?>>Click to select an SSL Report</option>
+            <option value="<?=$web_root?>/reporting/ssl/cost-by-category.php"<?php if ($report_name == "ssl-cost-by-category-report") echo " selected" ?>>Cost by Category</option>
             <option value="<?=$web_root?>/reporting/ssl/cost-by-month.php"<?php if ($report_name == "ssl-cost-by-month-report") echo " selected" ?>>Cost by Month</option>
             <option value="<?=$web_root?>/reporting/ssl/ssl-provider-fees.php"<?php if ($report_name == "ssl-provider-fee-report") echo " selected" ?>>SSL Provider Fees</option>
             <option value="<?=$web_root?>/reporting/ssl/renewals.php"<?php if ($report_name == "ssl-renewal-report") echo " selected" ?>>Renewals</option>
