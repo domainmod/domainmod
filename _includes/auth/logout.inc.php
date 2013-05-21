7097c59d758cd9d706951f5dd3b8bcb7c9ace272
Greg Chetcuti <greg@chetcuti.com>
2013-05-20 21:17:55 -0700

    Update logout process

--- _includes/auth/logout.inc.php
+++ _includes/auth/logout.inc.php
@@ -21,6 +21,15 @@ include("auth-check.inc.php");
 
 $_SESSION = array();
 
+if (ini_get("session.use_cookies")) {
+
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+
+}
+
+session_destroy();
+
 header("Location: ../../");
 exit;
 ?>
