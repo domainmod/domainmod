f5b365fef544e26b632b24860feca66152100d7b
Greg Chetcuti <greg@chetcuti.com>
2013-06-01 19:08:39 -0700

    Database Update v2.005

--- _includes/software.inc.php
+++ _includes/software.inc.php
@@ -1,26 +1,28 @@
 <?php
 // /_includes/software.inc.php
 // 
-// Domain Manager - A web-based application written in PHP & MySQL used to manage a collection of domain names.
+// DomainMOD - A web-based application written in PHP & MySQL used to manage a collection of domain names.
 // Copyright (C) 2010 Greg Chetcuti
 // 
-// Domain Manager is free software; you can redistribute it and/or modify it under the terms of the GNU General
+// DomainMOD is free software; you can redistribute it and/or modify it under the terms of the GNU General
 // Public License as published by the Free Software Foundation; either version 2 of the License, or (at your
 // option) any later version.
 // 
-// Domain Manager is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// DomainMOD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
 // implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
 // for more details.
 // 
-// You should have received a copy of the GNU General Public License along with Domain Manager. If not, please 
-// see http://www.gnu.org/licenses/
+// You should have received a copy of the GNU General Public License along with DomainMOD. If not, please see
+// http://www.gnu.org/licenses/
 ?>
 <?php
-$software_title = "Domain Manager"; 
-$reporting_section_title = "Domain & SSL Certificate Reporting"; 
+$software_title = "DomainMOD"; 
+$software_title_slug = "domainmod"; 
+$reporting_section_title = "Reporting"; 
 $software_version = "1.0"; 
 $site_width = "96%";
 $site_width_login = "550";
-$most_recent_db_version = "2.0049";
+$code_home_url = "http://domainmod.org";
+$most_recent_db_version = "2.005";
 header('Content-Type: text/html; charset=utf-8');
 ?>
