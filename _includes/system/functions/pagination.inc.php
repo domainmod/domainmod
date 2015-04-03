a62758be01f9a7ecbf9916dde237774dc41b615a
Greg Chetcuti <greg@chetcuti.com>
2015-04-03 13:24:58 -0700

    Cleanup code based on Codacy findings

--- _includes/system/functions/pagination.inc.php
+++ _includes/system/functions/pagination.inc.php
@@ -20,92 +20,110 @@
  */
 ?>
 <?php
-function pageBrowser($totalrows,$numLimit,$amm,$queryStr,$numBegin,$begin,$num) {
+function pageBrowser($totalrows, $numLimit, $amm, $queryStr, $numBegin, $begin, $num) {
 
-	$larrow = "&nbsp;&laquo; Prev &nbsp;";
-	$rarrow = "&nbsp;Next &raquo;&nbsp;";
-	$wholePiece = "<B>Page:</B> ";
-	if ($totalrows > 0) {
-		$numSoFar = 1;
-		$cycle = ceil($totalrows/$amm);
-		
-		if (!isset($numBegin) || $numBegin < 1) {
-			$numBegin = 1;
+    $larrow = "&nbsp;&laquo; Prev &nbsp;";
+    $rarrow = "&nbsp;Next &raquo;&nbsp;";
+    $wholePiece = "<B>Page:</B> ";
+
+    if ($totalrows > 0) {
+
+        $numSoFar = 1;
+        $cycle = ceil($totalrows/$amm);
+
+        if (!isset($numBegin) || $numBegin < 1) {
+
+            $numBegin = 1;
 			$num = 1;
-		}
-
-		$minus = $numBegin-1;
-		$start = $minus*$amm;
-
-		if (!isset($begin)) {
-			$begin = $start;
-		}
-
-		$preBegin = $numBegin-$numLimit;
-		$preStart = $amm*$numLimit;
-		$preStart = $start-$preStart;
-		$preVBegin = $start-$amm;
-		$preRedBegin = $numBegin-1;
-
-		if ($start > 0 || $numBegin > 1) {
-			$wholePiece .= "<a href='?num=".$preRedBegin
-					."&numBegin=".$preBegin
-					."&begin=".$preVBegin
-					.$queryStr."'>"
-					.$larrow."</a>\n";
-		}
-
-		for ($i=$numBegin;$i<=$cycle;$i++) {
-			if ($numSoFar == $numLimit+1) {
-				$piece = "<a href='?numBegin=".$i
-					."&num=".$i
-					."&begin=".$start
-					.$queryStr."'>"
-					.$rarrow."</a>\n";
-				$wholePiece .= $piece;
-				break;
-			}
-
-			$piece = "<a href='?begin=".$start
-				."&num=".$i
-				."&numBegin=".$numBegin
-				.$queryStr
-				."'>";
-
-			if ($num == $i) {
-				$piece .= "</a><b>$i</b><a>";
-			} else {
-				$piece .= "$i";
-			}
-
-			$piece .= "</a>\n";
-			$start = $start+$amm;
-			$numSoFar++;
-			$wholePiece .= $piece;
 
 		}
 
-		$wholePiece .= "\n";
-		$wheBeg = $begin+1;
-		$wheEnd = $begin+$amm;
-		$wheToWhe = "<b>".number_format($wheBeg)."</b>-<b>";
+        $minus = $numBegin-1;
+        $start = $minus*$amm;
 
-		if ($totalrows <= $wheEnd) {
-			$wheToWhe .= $totalrows."</b>";
-		} else {
-			$wheToWhe .= number_format($wheEnd)."</b>";
-		}
+        if (!isset($begin)) {
 
-		$sqlprod = " LIMIT ".$begin.", ".$amm;
+            $begin = $start;
 
-	} else {
+        }
 
-		$wholePiece = "";
-		$wheToWhe = "<b>0</b> - <b>0</b>";
+        $preBegin = $numBegin-$numLimit;
+        $preStart = $amm*$numLimit;
+        $preStart = $start-$preStart;
+        $preVBegin = $start-$amm;
+        $preRedBegin = $numBegin-1;
 
-	}
+        if ($start > 0 || $numBegin > 1) {
 
-	return array($sqlprod,$wheToWhe,$wholePiece);
+            $wholePiece .= "<a href='?num=" . $preRedBegin
+                . "&numBegin=" . $preBegin
+                . "&begin=" . $preVBegin
+                . $queryStr . "'>"
+                . $larrow . "</a>\n";
+
+        }
+
+        for ($i=$numBegin; $i<=$cycle; $i++) {
+
+            if ($numSoFar == $numLimit+1) {
+
+                $piece = "<a href='?numBegin=" . $i
+                    . "&num=" . $i
+                    . "&begin=" . $start
+                    . $queryStr . "'>"
+                    . $rarrow . "</a>\n";
+                $wholePiece .= $piece;
+                break;
+
+            }
+
+            $piece = "<a href='?begin=" . $start
+                . "&num=" . $i
+                . "&numBegin=" . $numBegin
+                . $queryStr
+                . "'>";
+
+            if ($num == $i) {
+
+                $piece .= "</a><b>$i</b><a>";
+
+            } else {
+
+                $piece .= "$i";
+
+            }
+
+            $piece .= "</a>\n";
+            $start = $start+$amm;
+            $numSoFar++;
+            $wholePiece .= $piece;
+
+        }
+
+        $wholePiece .= "\n";
+        $wheBeg = $begin+1;
+        $wheEnd = $begin+$amm;
+        $wheToWhe = "<b>" . number_format($wheBeg) . "</b>-<b>";
+
+        if ($totalrows <= $wheEnd) {
+
+            $wheToWhe .= $totalrows . "</b>";
+
+        } else {
+
+            $wheToWhe .= number_format($wheEnd) . "</b>";
+
+        }
+
+        $sqlprod = " LIMIT " . $begin . ", " . $amm;
+
+    } else {
+
+        $wholePiece = "";
+        $wheToWhe = "<b>0</b> - <b>0</b>";
+
+    }
+
+    return array($sqlprod,$wheToWhe,$wholePiece);
 
 }
-?>
