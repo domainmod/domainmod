ca1e13ee7b7ba4794886f96629c2d5c7fca7d308
Greg Chetcuti <greg@chetcuti.com>
2015-04-02 23:36:29 -0700

    Cleanup code based on Codacy findings

--- add/index.php
+++ add/index.php
@@ -1,21 +1,20 @@
 <?php
 // /add/index.php
-// 
+//
 // DomainMOD is an open source application written in PHP & MySQL used to track and manage your web resources.
 // Copyright (C) 2010 Greg Chetcuti
-// 
+//
 // DomainMOD is free software; you can redistribute it and/or modify it under the terms of the GNU General
 // Public License as published by the Free Software Foundation; either version 2 of the License, or (at your
 // option) any later version.
-// 
+//
 // DomainMOD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
 // implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
 // for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License along with DomainMOD. If not, please see
 // http://www.gnu.org/licenses/
 ?>
 <?php
 header("Location: ../invalid.php");
 exit;
-?>
