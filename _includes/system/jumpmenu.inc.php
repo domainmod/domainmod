4dcf4903cfcbf171cf5ee878b3e698d1a53deb60
Greg Chetcuti <greg@chetcuti.com>
2015-04-22 00:41:52 -0400

    Move the jumpmenu include

--- _includes/system/jumpmenu.inc.php
+++ _includes/system/jumpmenu.inc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * /_includes/system/functions/jumpmenu.inc.php
+ * /_includes/system/jumpmenu.inc.php
  *
  * This file is part of DomainMOD, an open source domain and internet asset manager.
  * Copyright (C) 2010-2015 Greg Chetcuti <greg@chetcuti.com>
