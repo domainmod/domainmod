7501717e60bd7da9b6661222279946ef7a5f7c36
Greg Chetcuti <greg@chetcuti.com>
2015-04-25 13:10:16 -0400

    Add root and include directory definitions

--- _includes/software.inc.php
+++ _includes/software.inc.php
@@ -28,4 +28,6 @@ $site_width = "96%";
 $site_width_login = "550";
 $code_home_url = "http://domainmod.org";
 $most_recent_db_version = "2.0057";
+define('DIR_ROOT', dirname(dirname(__FILE__)) . '/');
+define('DIR_INC', DIR_ROOT . '_includes/');
 header('Content-Type: text/html; charset=utf-8');
