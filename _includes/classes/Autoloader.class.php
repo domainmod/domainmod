043d0e63d498c44f77ac88d829efb3057231b3e1
Greg Chetcuti <greg@chetcuti.com>
2015-05-10 23:56:25 -0400

    Add Autoloader class

---
+++ _includes/classes/Autoloader.class.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * /_includes/classes/Autoloader.class.php
+ *
+ * This file is part of DomainMOD, an open source domain and internet asset manager.
+ * Copyright (C) 2010-2015 Greg Chetcuti <greg@chetcuti.com>
+ *
+ * Project: http://domainmod.org   Author: http://chetcuti.com
+ *
+ * DomainMOD is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * DomainMOD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with DomainMOD. If not, see
+ * http://www.gnu.org/licenses/.
+ *
+ */
+?>
+<?php
+namespace DomainMOD;
+
+class Autoloader
+{
+
+    static public function classAutoloader($class_full)
+    {
+
+        $class_path = DIR_INC . "classes/";
+        $class_base = preg_replace("/DomainMOD\\\/", "", $class_full);
+        require_once($class_path . $class_base . '.class.php');
+
+    }
+
+}
