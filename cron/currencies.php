08d49cf5f30265ca1e3976788f71f270ad0f5af7
Greg Chetcuti <greg@chetcuti.com>
2015-06-23 17:17:15 -0700

    Update to use the Conversion->updateRates method

--- cron/currencies.php
+++ cron/currencies.php
@@ -21,241 +21,32 @@
 ?>
 <?php
 include("../_includes/init.inc.php");
-include(DIR_INC . "head.inc.php");
-include(DIR_INC . "software.inc.php");
-include(DIR_INC . "config.inc.php");
-include(DIR_INC . "database.inc.php");
-require_once(DIR_ROOT . "classes/Autoloader.php");
 
+require_once(DIR_ROOT . "classes/Autoloader.php");
 spl_autoload_register('DomainMOD\Autoloader::classAutoloader');
 
+$conversion = new DomainMOD\Conversion();
 $error = new DomainMOD\Error();
 $time = new DomainMOD\Timestamp();
 $timestamp = $time->time();
 
+include(DIR_INC . "head.inc.php");
+include(DIR_INC . "software.inc.php");
+include(DIR_INC . "config.inc.php");
+include(DIR_INC . "database.inc.php");
+
 include(DIR_INC . "config-demo.inc.php");
 
-if ($demo_install != "1") {
+if ($demo_install != '1') {
 
-	$sql = "SELECT c.id, c.currency
-			FROM currencies AS c, fees AS f, domains AS d
-			WHERE c.id = f.currency_id
-			  AND f.id = d.fee_id
-			  AND d.active NOT IN ('0', '10')
-			  GROUP BY c.currency";
-	$result = mysqli_query($connection, $sql) or $error->outputOldSqlError($connection);
-	
-	while ($row = mysqli_fetch_object($result)) {
-	
-		$exclude_string .= "'" . $row->currency . "', ";
-	
-		$sql_user = "SELECT user_id, default_currency
-					 FROM user_settings";
-		$result_user = mysqli_query($connection, $sql_user);
-		
-		while ($row_user = mysqli_fetch_object($result_user)) {
-	
-			$sql_existing = "SELECT id
-							 FROM currency_conversions
-							 WHERE currency_id = '" . $row->id . "'
-							   AND user_id = '" . $row_user->user_id . "'";
-			$result_existing = mysqli_query($connection, $sql_existing) or $error->outputOldSqlError($connection);
-			
-			if (mysqli_num_rows($result_existing) == 0) {
-				
-				$existing_currency = "";
-				
-			} else {
-				
-				$existing_currency = "1";
-				
-			}
-		
-			if ($existing_currency == "1") {
-		
-				if ($row->currency == $row_user->default_currency) {
-		
-					$sql_update = "UPDATE currency_conversions
-								   SET conversion = '1',
-									   update_time = '" . $timestamp . "'
-								   WHERE currency_id = '" . $row->id . "'
-									 AND user_id = '" . $row_user->user_id . "'";
-					$result_update = mysqli_query($connection, $sql_update) or $error->outputOldSqlError($connection);
-		
-				} else {
-		
-					$from = $row->currency;
-					$to = $row_user->default_currency;
-					$full_url = "http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=" . $from . $to ."=X";
-					$api_call = @fopen($full_url, "r");
-				
-					if ($api_call) {
-				
-						$api_call_result = fgets($api_call, 4096);
-						fclose($api_call);
-				
-					}
-					
-					$api_call_split = explode(",", $api_call_result);
-					$conversion_rate = $api_call_split[1];
-		
-					$sql_update = "UPDATE currency_conversions
-								   SET conversion = '" . $conversion_rate . "',
-									   update_time = '" . $timestamp . "'
-								   WHERE currency_id = '" . $row->id . "'
-									 AND user_id = '" . $row_user->user_id . "'";
-					$result_update = mysqli_query($connection, $sql_update) or $error->outputOldSqlError($connection);
-		
-				}
-		
-			} else {
-		
-				if ($row->currency == $row_user->default_currency) {
-		
-					$sql_insert = "INSERT INTO currency_conversions
-								   (currency_id, user_id, conversion, insert_time, update_time) VALUES 
-								   ('" . $row->id . "', '" . $row_user->user_id . "', '1', '" . $timestamp . "', '" . $timestamp . "')";
-					$result_insert = mysqli_query($connection, $sql_insert) or $error->outputOldSqlError($connection);
-		
-				} else {
-		
-					$from = $row->currency;
-					$to = $row_user->default_currency;
-					$full_url = "http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=" . $from . $to ."=X";
-					$api_call = @fopen($full_url, "r");
-				
-					if ($api_call) {
-				
-						$api_call_result = fgets($api_call, 4096);
-						fclose($api_call);
-				
-					}
-					
-					$api_call_split = explode(",", $api_call_result);
-					$conversion_rate = $api_call_split[1];
-				
-					$sql_insert = "INSERT INTO currency_conversions
-								   (currency_id, user_id, conversion, insert_time, update_time) VALUES 
-								   ('" . $row->id . "', '" . $row_user->user_id . "', '" . $conversion_rate . "', '" . $timestamp . "', '" . $timestamp . "')";
-					$result_insert = mysqli_query($connection, $sql_insert) or $error->outputOldSqlError($connection);
-		
-				}
-		
-			}
-	
-		}
-	
-	}
-	
-	$exclude_string = substr($exclude_string, 0, -2);
-	
-	$sql = "SELECT c.id, c.currency
-			FROM currencies AS c, ssl_fees AS f, ssl_certs AS sslc
-			WHERE c.id = f.currency_id
-			  AND f.id = sslc.fee_id
-			  AND sslc.active NOT IN ('0')
-			  AND c.currency NOT IN (" . $exclude_string . ")
-			  GROUP BY c.currency";
-	$result = mysqli_query($connection, $sql) or $error->outputOldSqlError($connection);
-	
-	while ($row = mysqli_fetch_object($result)) {
-	
-		$sql_user = "SELECT user_id, default_currency
-					 FROM user_settings";
-		$result_user = mysqli_query($connection, $sql_user);
-		
-		while ($row_user = mysqli_fetch_object($result_user)) {
-	
-			$sql_existing = "SELECT id
-							 FROM currency_conversions
-							 WHERE currency_id = '" . $row->id . "'
-							   AND user_id = '" . $row_user->user_id . "'";
-			$result_existing = mysqli_query($connection, $sql_existing) or $error->outputOldSqlError($connection);
-			
-			if (mysqli_num_rows($result_existing) == 0) {
-				
-				$existing_currency = "";
-				
-			} else {
-				
-				$existing_currency = "1";
-				
-			}
-		
-			if ($existing_currency == "1") {
-		
-				if ($row->currency == $row_user->default_currency) {
-		
-					$sql_update = "UPDATE currency_conversions
-								   SET conversion = '1',
-									   update_time = '" . $timestamp . "'
-								   WHERE currency_id = '" . $row->id . "'
-									 AND user_id = '" . $row_user->user_id . "'";
-					$result_update = mysqli_query($connection, $sql_update) or $error->outputOldSqlError($connection);
-		
-				} else {
-		
-					$from = $row->currency;
-					$to = $row_user->default_currency;
-					$full_url = "http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=" . $from . $to ."=X";
-					$api_call = @fopen($full_url, "r");
-				
-					if ($api_call) {
-				
-						$api_call_result = fgets($api_call, 4096);
-						fclose($api_call);
-				
-					}
-					
-					$api_call_split = explode(",", $api_call_result);
-					$conversion_rate = $api_call_split[1];
-		
-					$sql_update = "UPDATE currency_conversions
-								   SET conversion = '" . $conversion_rate . "',
-									   update_time = '" . $timestamp . "'
-								   WHERE currency_id = '" . $row->id . "'
-									 AND user_id = '" . $row_user->user_id . "'";
-					$result_update = mysqli_query($connection, $sql_update) or $error->outputOldSqlError($connection);
-		
-				}
-		
-			} else {
-		
-				if ($row->currency == $row_user->default_currency) {
-		
-					$sql_insert = "INSERT INTO currency_conversions
-								   (currency_id, user_id, conversion, insert_time, update_time) VALUES 
-								   ('" . $row->id . "', '" . $row_user->user_id . "', '1', '" . $timestamp . "', '" . $timestamp . "')";
-					$result_insert = mysqli_query($connection, $sql_insert) or $error->outputOldSqlError($connection);
-		
-				} else {
-		
-					$from = $row->currency;
-					$to = $row_user->default_currency;
-					$full_url = "http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=" . $from . $to ."=X";
-					$api_call = @fopen($full_url, "r");
-				
-					if ($api_call) {
-				
-						$api_call_result = fgets($api_call, 4096);
-						fclose($api_call);
-				
-					}
-					
-					$api_call_split = explode(",", $api_call_result);
-					$conversion_rate = $api_call_split[1];
-				
-					$sql_insert = "INSERT INTO currency_conversions
-								   (currency_id, user_id, conversion, insert_time, update_time) VALUES 
-								   ('" . $row->id . "', '" . $row_user->user_id . "', '" . $conversion_rate . "', '" . $timestamp . "', '" . $timestamp . "')";
-					$result_insert = mysqli_query($connection, $sql_insert) or $error->outputOldSqlError($connection);
-		
-				}
-		
-			}
-	
-		}
-	
-	}
-	
+    $sql = "SELECT user_id, default_currency
+            FROM user_settings";
+    $result = mysqli_query($connection, $sql) or $error->outputOldSqlError($connection);
+
+    while ($row = mysqli_fetch_object($result)) {
+
+        $conversion->updateRates($connection, $timestamp, $row->default_currency, $row->user_id);
+
+    }
+
 }
