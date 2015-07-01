f5de659e9655c35391872dc32509c3bf31e4495e
Greg Chetcuti <greg@chetcuti.com>
2015-06-30 18:04:11 -0700

    Add the login check notice feature

---
+++ notice.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * /notice.php
+ *
+ * This file is part of DomainMOD, an open source domain and internet asset manager.
+ * Copyright (C) 2010-2015 Greg Chetcuti <greg@chetcuti.com>
+ *
+ * Project: http://domainmod.org   Author: http://chetcuti.com
+ *
+ * DomainMOD is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * DomainMOD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with DomainMOD. If not, see
+ * http://www.gnu.org/licenses/.
+ *
+ */
+?>
+<?php
+include("_includes/start-session.inc.php");
+include("_includes/init.inc.php");
+
+require_once(DIR_ROOT . "classes/Autoloader.php");
+spl_autoload_register('DomainMOD\Autoloader::classAutoloader');
+
+$system = new DomainMOD\System();
+
+include(DIR_INC . "head.inc.php");
+include(DIR_INC . "config.inc.php");
+include(DIR_INC . "software.inc.php");
+include(DIR_INC . "database.inc.php");
+
+$system->authCheck();
+
+$action = $_GET['a'];
+
+if ($action = 'u') { // u = upgrade DomainMOD
+
+    $_SESSION['notice_page_tile'] = "Upgrade Available";
+    $_SESSION['notice'] = "<center>Your " . $software_title . " software was recently updated, so we now need to upgrade
+    your database<BR><BR><a href='checks.php?u=1'>Click here to upgrade your database</a></center>";
+
+}
+$page_title = $_SESSION['notice_page_title'];
+$software_section = "notice";
+?>
+<?php include(DIR_INC . 'doctype.inc.php'); ?>
+<html>
+<head>
+    <title><?php echo $system->pageTitle($software_title, $page_title); ?></title>
+    <?php include(DIR_INC . "layout/head-tags.inc.php"); ?>
+</head>
+<body>
+<?php include(DIR_INC . "layout/header.inc.php"); ?>
+<?php
+echo $_SESSION['notice'];
+?><BR><BR>
+<?php include(DIR_INC . "layout/footer.inc.php"); ?>
+</body>
+</html>
