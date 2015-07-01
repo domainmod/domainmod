5c4af1765ff877507789dbe95614a6436117651c
Greg Chetcuti <greg@chetcuti.com>
2015-07-01 01:22:02 -0700

    Cleanup the DwDisplay class

--- classes/DomainMOD/DwDisplay.php
+++ classes/DomainMOD/DwDisplay.php
@@ -28,14 +28,7 @@ class DwDisplay
     public function account($connection, $server_id, $domain, $show_sidebar, $show_domain, $show_zone)
     {
 
-        $sql = "SELECT a.unix_startdate, a.email, a.ip, a.plan, a.theme, a.`user`, a.`owner`, a.shell, a.`partition`,
-                    a.disklimit, a.diskused, a.maxpop, a.maxlst, a.maxaddons, a.maxsub, a.maxsql, a.maxftp, a.maxparked,
-                    a.suspended, a.suspendtime, a.suspendreason, s.name AS server_name
-                FROM dw_accounts AS a, dw_servers AS s
-                WHERE a.server_id = s.id
-                  AND a.domain = '" . $domain . "'
-                  AND a.server_id = '" . $server_id . "'";
-        $result_account = mysqli_query($connection, $sql);
+        $result_account = $this->getAccount($connection, $domain, $server_id);
 
         $result = '';
 
@@ -45,43 +38,37 @@ class DwDisplay
 
             <tr class="main_table_row_active_no_hover"><?php
 
-                if ($show_sidebar == '1') {
+            echo $this->sidebar($domain, $server_id, $row->server_name, $show_domain, $show_zone, $show_sidebar); ?>
 
-                    echo $this->sidebar($domain, $server_id, $row->server_name, $show_domain, $show_zone);
-
-                } ?>
-
-                <td class="main_table_cell_active_top_aligned">
-                    <strong>Created:</strong> <?php echo date("Y M jS", $row->unix_startdate); ?><BR>
-                    <strong>Contact:</strong> <?php echo $row->email; ?><BR>
-                    <strong>IP Address:</strong> <?php echo $row->ip; ?><BR>
-                    <strong>Hosting Plan:</strong> <?php echo $row->plan; ?><BR>
-                    <strong>cPanel Theme:</strong> <?php echo $row->theme; ?><BR>
-                    <strong>User, Owner:</strong> <?php echo $row->user; ?>, <?php echo $row->owner; ?>
-                </td>
-                <td class="main_table_cell_active_top_aligned">
-                    <strong>Shell:</strong> <?php echo $row->shell; ?><BR>
-                    <strong>Home:</strong> /<?php echo $row->partition; ?> /<?php echo $row->user; ?><BR>
-                    <strong>HD
-                        Quota:</strong> <?php echo $row->disklimit; ?>
-                        <?php if ($row->disklimit != "unlimited") echo " MB"; ?>
-                        <BR>
-                    <strong>HD Used:</strong> <?php echo number_format($row->diskused); ?> MB
-                </td>
-                <td class="main_table_cell_active_top_aligned">
-                    <strong>POP:</strong> <?php echo $row->maxpop; ?><BR>
-                    <strong>Lists:</strong> <?php echo $row->maxlst; ?><BR>
-                    <strong>Addons:</strong> <?php echo $row->maxaddons; ?><BR>
-                    <strong>Subdomains:</strong> <?php echo $row->maxsub; ?><BR>
-                    <strong>SQL:</strong> <?php echo $row->maxsql; ?><BR>
-                    <strong>FTP:</strong> <?php echo $row->maxftp; ?><BR>
-                    <strong>Parked:</strong> <?php echo $row->maxparked; ?><BR><BR>
-                </td>
-                <td class="main_table_cell_active_top_aligned">
-                    <strong>Suspended?</strong> <?php echo $row->suspended; ?><BR>
-                    <strong>When?</strong> <?php echo $row->suspendtime; ?><BR>
-                    <strong>Why?</strong> <?php echo $row->suspendreason; ?>
-                </td>
+            <td class="main_table_cell_active_top_aligned">
+                <strong>Created:</strong> <?php echo date("Y M jS", $row->unix_startdate); ?><BR>
+                <strong>Contact:</strong> <?php echo $row->email; ?><BR>
+                <strong>IP Address:</strong> <?php echo $row->ip; ?><BR>
+                <strong>Hosting Plan:</strong> <?php echo $row->plan; ?><BR>
+                <strong>cPanel Theme:</strong> <?php echo $row->theme; ?><BR>
+                <strong>User, Owner:</strong> <?php echo $row->user; ?>, <?php echo $row->owner; ?>
+            </td>
+            <td class="main_table_cell_active_top_aligned">
+                <strong>Shell:</strong> <?php echo $row->shell; ?><BR>
+                <strong>Home:</strong> /<?php echo $row->partition; ?> /<?php echo $row->user; ?><BR>
+                <strong>HD Quota:</strong> <?php echo $row->disklimit; ?>
+                <?php if ($row->disklimit != "unlimited") echo " MB"; ?><BR>
+                <strong>HD Used:</strong> <?php echo number_format($row->diskused); ?> MB
+            </td>
+            <td class="main_table_cell_active_top_aligned">
+                <strong>POP:</strong> <?php echo $row->maxpop; ?><BR>
+                <strong>Lists:</strong> <?php echo $row->maxlst; ?><BR>
+                <strong>Addons:</strong> <?php echo $row->maxaddons; ?><BR>
+                <strong>Subdomains:</strong> <?php echo $row->maxsub; ?><BR>
+                <strong>SQL:</strong> <?php echo $row->maxsql; ?><BR>
+                <strong>FTP:</strong> <?php echo $row->maxftp; ?><BR>
+                <strong>Parked:</strong> <?php echo $row->maxparked; ?><BR><BR>
+            </td>
+            <td class="main_table_cell_active_top_aligned">
+                <strong>Suspended?</strong> <?php echo $row->suspended; ?><BR>
+                <strong>When?</strong> <?php echo $row->suspendtime; ?><BR>
+                <strong>Why?</strong> <?php echo $row->suspendreason; ?>
+            </td>
             </tr><?php
 
             $result = ob_get_clean(); //@formatter:on
@@ -92,31 +79,77 @@ class DwDisplay
 
     }
 
-    public function sidebar($domain, $server_id, $server_name, $show_domain, $show_zone)
+    public function getAccount($connection, $domain, $server_id)
     {
 
-        $visible_domain = wordwrap($domain, 20, "<BR>", true);
+        $sql = "SELECT a.unix_startdate, a.email, a.ip, a.plan, a.theme, a.`user`, a.`owner`, a.shell, a.`partition`,
+                    a.disklimit, a.diskused, a.maxpop, a.maxlst, a.maxaddons, a.maxsub, a.maxsql, a.maxftp, a.maxparked,
+                    a.suspended, a.suspendtime, a.suspendreason, s.name AS server_name
+                FROM dw_accounts AS a, dw_servers AS s
+                WHERE a.server_id = s.id
+                  AND a.domain = '" . $domain . "'
+                  AND a.server_id = '" . $server_id . "'";
 
-        ob_start(); //@formatter:off ?>
+        return mysqli_query($connection, $sql);
 
-        <td class="main_table_cell_active_top_aligned"><?php
+    }
 
-            if ($show_domain == '1') { ?>
-                <strong><?php echo $visible_domain; ?></strong><BR><BR><?php
-            }
+    public function sidebar($domain, $server_id, $server_name, $show_domain, $show_zone, $show_sidebar)
+    {
 
-            echo $server_name . "<BR>";
+        ob_start(); //@formatter:off
 
-            if ($show_zone == '1') { ?>
+        $result = '';
 
-                [<a class="covert_link" href="list-dns-zones.php?domain=<?php echo $domain; ?>&server_id=<?php echo
-                $server_id; ?>">dns zone</a>]<?php
+        if ($show_sidebar == '1') { ?>
 
-            } ?>
+            <td class="main_table_cell_active_top_aligned"><?php
 
-        </td><?php
+                echo $this->showDomain($domain, $show_domain);
 
-        return ob_get_clean(); //@formatter:on
+                echo $server_name . "<BR>";
+
+                echo $this->showZoneLink($domain, $server_id, $show_zone); ?>
+
+            </td><?php
+
+            $result = ob_get_clean();
+
+        }
+
+        return $result; //@formatter:on
+
+    }
+
+    public function showDomain($domain, $show_domain)
+    {
+
+        $result = '';
+
+        if ($show_domain == '1') {
+
+            $visible_domain = wordwrap($domain, 20, "<BR>", true);
+
+            $result = "<strong>" . $visible_domain . "</strong><BR><BR>";
+
+        }
+
+        return $result;
+    }
+
+    public function showZoneLink($domain, $server_id, $show_zone)
+    {
+
+        $result = '';
+
+        if ($show_zone == '1') {
+
+            $result = "[<a class=\"covert_link\" href=\"list-dns-zones.php?domain=<?php echo $domain
+            ?>&server_id=$server_id\">dns zone</a>]";
+
+        }
+
+        return $result;
 
     }
 
