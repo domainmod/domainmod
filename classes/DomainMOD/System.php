01a97a34309ed15d7b1d0d4a29d688b9429463f7
Greg Chetcuti <greg@chetcuti.com>
2015-07-05 04:19:50 -0700

    Refactor System class

--- classes/DomainMOD/System.php
+++ classes/DomainMOD/System.php
@@ -26,52 +26,37 @@ class System
 {
 
     public function installCheck($connection, $web_root)
-    {
-
+    { //@formatter:off
         $full_install_path = DIR_ROOT . "install/";
-
         if (is_dir($full_install_path) &&
-
-            !mysqli_num_rows(mysqli_query($connection, "SHOW TABLES LIKE '" . `dw_servers` . "'"))
-        ) {
-
+            !mysqli_num_rows(mysqli_query($connection, "SHOW TABLES LIKE '" . `dw_servers` . "'"))) {
             $installation_mode = 1;
             $result_message = "<a href=\"" . $web_root . "/install/\">Click here to install</a><BR>";
-
         } else {
-
             $installation_mode = 0;
             $result_message = '';
-
         }
-
         return array($installation_mode, $result_message);
-
-    }
+    } //@formatter:on
 
     public function checkVersion($connection, $current_version)
     {
-
-        $live_version
-            = file_get_contents('https://raw.githubusercontent.com/domainmod/domainmod/master/version.txt');
-
+        $live_version = $this->getLiveVersion();
         if ($current_version < $live_version && $live_version != '') {
-
             $sql = "UPDATE settings SET upgrade_available = '1'";
             mysqli_query($connection, $sql);
-
             $_SESSION['system_upgrade_available'] = '1';
             $message = $this->getUpgradeMessage();
-
         } else {
-
             $_SESSION['system_upgrade_available'] = '0';
             $message = "No Upgrade Available";
-
         }
-
         return $message;
+    }
 
+    public function getLiveVersion()
+    {
+        return file_get_contents('https://raw.githubusercontent.com/domainmod/domainmod/master/version.txt');
     }
 
     public function getUpgradeMessage()
@@ -82,122 +67,80 @@ class System
 
     public function pageTitle($software_title, $page_title)
     {
-
         return $software_title . " :: " . $page_title;
-
     }
 
     public function pageTitleSub($software_title, $page_title, $page_subtitle)
     {
-
         return $software_title . " :: " . $page_title . " :: " . $page_subtitle;
-
     }
 
     public function checkExistingAssets($connection)
     {
-
         $queryB = new QueryBuild();
 
         $sql = $queryB->singleAsset('registrars');
         $_SESSION['has_registrar'] = $this->checkForRows($connection, $sql);
-
         $sql = $queryB->singleAsset('registrar_accounts');
         $_SESSION['has_registrar_account'] = $this->checkForRows($connection, $sql);
-
         $sql = $queryB->singleAsset('domains');
         $_SESSION['has_domain'] = $this->checkForRows($connection, $sql);
-
         $sql = $queryB->singleAsset('ssl_providers');
         $_SESSION['has_ssl_provider'] = $this->checkForRows($connection, $sql);
-
         $sql = $queryB->singleAsset('ssl_accounts');
         $_SESSION['has_ssl_account'] = $this->checkForRows($connection, $sql);
-
         $sql = $queryB->singleAsset('ssl_certs');
         $_SESSION['has_ssl_cert'] = $this->checkForRows($connection, $sql);
-
         return true;
-
     }
 
     public function checkForRows($connection, $sql)
-    {
+    { //@formatter:off
         $result = mysqli_query($connection, $sql);
-        if (mysqli_num_rows($result) >= 1) {
-            return '1';
-        } else {
-            return '0';
-        }
-    }
+        if (mysqli_num_rows($result) >= 1) { return '1'; } else { return '0'; }
+    } //@formatter:on
 
     public function checkForRowsResult($connection, $sql)
-    {
+    { //@formatter:off
         $result = mysqli_query($connection, $sql);
-        if (mysqli_num_rows($result) >= 1) {
-            return $result;
-        } else {
-            return '0';
-        }
-    }
+        if (mysqli_num_rows($result) >= 1) { return $result; } else { return '0'; }
+    } //@formatter:on
 
     public function authCheck()
     {
-
         if ($_SESSION['is_logged_in'] != 1) {
-
             $_SESSION['user_redirect'] = $_SERVER["REQUEST_URI"];
-
             $_SESSION['result_message'] = "You must be logged in to access this area<BR>";
-
             header("Location: " . $_SESSION['web_root'] . "/");
             exit;
-
         }
-
     }
 
     public function loginCheck()
     {
-
         if ($_SESSION['is_logged_in'] == 1) {
-
             header("Location: " . $_SESSION['web_root'] . "/domains.php");
             exit;
-
         }
-
     }
 
     public function checkAdminUser($is_admin, $web_root)
     {
-
         if ($is_admin !== 1) {
-
             header("Location: " . $web_root . "/invalid.php");
             exit;
-
         }
-
     }
 
     public function showResultMessage($result_message)
-    {
-
-        //@formatter:off
+    { //@formatter:off
         ob_start(); ?>
-
         <div class="result_message_outer">
             <div class="result_message_inner">
                 <?php echo $result_message; ?>
             </div>
         </div><?php
-        //@formatter:on
-
-        $result = ob_get_clean();
-
-        return $result;
-
-    }
+        return ob_get_clean();
+    } //@formatter:on
 
 }
