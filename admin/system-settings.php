f161f50872849aabeefa882bbdd32aa9b17e140d
Greg Chetcuti <greg@chetcuti.com>
2015-10-10 12:09:32 -0700

    Update redirect URL after updating System Settings
    After a successful update to the System Settings, users were being
    redirected to an invalid URL.

--- admin/system-settings.php
+++ admin/system-settings.php
@@ -75,7 +75,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && $new_email_address != "" && $new_ful
 
     $_SESSION['s_result_message'] .= "The System Settings were updated<BR>";
 
-    header("Location: index.php");
+    header("Location: ../settings/");
     exit;
 
 } else {
