0bbc32d9fe4150708824618fe26f110492dcdcd0
Greg Chetcuti <greg@chetcuti.com>
2016-03-16 19:34:57 -0700

    Cleanup form inputs
    Add htmlentities to appropriate inputs

--- classes/DomainMOD/Form.php
+++ classes/DomainMOD/Form.php
@@ -49,7 +49,7 @@ class Form
             <div class="form-group">
                 <label><?php echo $text_to_display; ?><?php if ($subtext != '') echo '<BR><span style="font-weight: normal;">' . $subtext . '</span><BR>'; ?></label>
                 <input type="<?php if ($is_password == '1') { echo "password"; } else { echo "text"; } ?>" class="form-control" placeholder="<?php echo $text_to_display; ?>" name="<?php echo $name; ?>"
-                    value="<?php echo $value; ?>" maxlength="<?php echo $maxlength; ?>">
+                    value="<?php echo htmlentities($value, ENT_QUOTES); ?>" maxlength="<?php echo $maxlength; ?>">
             </div><?php
             echo $after;
         return ob_get_clean();
@@ -71,7 +71,7 @@ class Form
     public function showInputHidden($name, $value)
     {
         ob_start(); ?>
-            <input type="hidden" class="form-control" name="<?php echo $name; ?>" value="<?php echo $value; ?>"><?php
+            <input type="hidden" class="form-control" name="<?php echo $name; ?>" value="<?php echo htmlentities($value, ENT_QUOTES); ?>"><?php
         return ob_get_clean();
     }
 
