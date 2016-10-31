becff147c96b32e807e01f822d913aa6c60feb6c
Greg Chetcuti <greg@chetcuti.com>
2016-10-30 21:39:30 -0700

    Update Composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -16,10 +16,22 @@ class ComposerStaticInitb2ab50c1bc2ddce3c955d66d9b575d56
         ),
     );
 
+    public static $classMap = array (
+        'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
+        'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+        'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
+        'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
+        'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
+        'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
+        'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
+        'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixesPsr0 = ComposerStaticInitb2ab50c1bc2ddce3c955d66d9b575d56::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInitb2ab50c1bc2ddce3c955d66d9b575d56::$classMap;
 
         }, null, ClassLoader::class);
     }
