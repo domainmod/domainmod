a0170b7579aa2245d5b825c250f8ed0a2f2cd9ab
Greg Chetcuti <greg@chetcuti.com>
2017-01-01 14:05:45 -0800

    Update Copyright to 2017

--- _includes/breadcrumbs/queue-add.inc.php
+++ _includes/breadcrumbs/queue-add.inc.php
@@ -3,7 +3,7 @@
  * /_includes/breadcrumbs/queue-add.inc.php
  *
  * This file is part of DomainMOD, an open source domain and internet asset manager.
- * Copyright (c) 2010-2016 Greg Chetcuti <greg@chetcuti.com>
+ * Copyright (c) 2010-2017 Greg Chetcuti <greg@chetcuti.com>
  *
  * Project: http://domainmod.org   Author: http://chetcuti.com
  *
