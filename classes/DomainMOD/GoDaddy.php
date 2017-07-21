486c48bcfaa56801f357733a2ada838d5bf26640
lucentminds <scott@lifecorp.com>
2017-07-21 17:00:32 -0500

    Git it right. :(

--- classes/DomainMOD/GoDaddy.php
+++ classes/DomainMOD/GoDaddy.php
@@ -5,7 +5,7 @@
  * This file is part of DomainMOD, an open source domain and internet asset manager.
  * Copyright (c) 2010-2017 Greg Chetcuti <greg@chetcuti.com>
  *
- * Project: http://domainmod.org       Author: http://chetcuti.com
+ * Project: http://domainmod.org   Author: http://chetcuti.com
  *
  * DomainMOD is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
@@ -23,157 +23,156 @@ namespace DomainMOD;
 
 class GoDaddy
 {
-       public $format;
-       public $log;
+    public $format;
+    public $log;
 
-       public function __construct()
-       {
-              $this->format = new Format();
-              $this->log = new Log('godaddy.class');
-       }
+    public function __construct()
+    {
+        $this->format = new Format();
+        $this->log = new Log('godaddy.class');
+    }
 
-       public function getApiUrl($domain, $command)
-       {
-              $base_url = 'https://api.godaddy.com/v1/';
-              if ($command == 'domainlist') {
-                     return $base_url . 'domains?statusGroups=VISIBLE&limit=10000';
-              } elseif ($command == 'info') {
-                     return $base_url . 'domains/' . $domain;
-              } else {
-                     return 'Unable to build API URL';
-              }
-       }
+    public function getApiUrl($domain, $command)
+    {
+        $base_url = 'https://api.godaddy.com/v1/';
+        if ($command == 'domainlist') {
+            return $base_url . 'domains?statusGroups=VISIBLE&limit=10000';
+        } elseif ($command == 'info') {
+            return $base_url . 'domains/' . $domain;
+        } else {
+            return 'Unable to build API URL';
+        }
+    }
 
-       public function apiCall($api_key, $api_secret, $full_url)
-       {
-              $handle = curl_init($full_url);
-              curl_setopt($handle, CURLOPT_HTTPHEADER, array(
-                     'Authorization: sso-key ' . $api_key . ':' . $api_secret,
-                     'Accept: application/json'));
-              curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, 0);
-              curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
-              $result = curl_exec($handle);
-              $cError = curl_error( $handle );
+    public function apiCall($api_key, $api_secret, $full_url)
+    {
+        $handle = curl_init($full_url);
+        curl_setopt($handle, CURLOPT_HTTPHEADER, array(
+            'Authorization: sso-key ' . $api_key . ':' . $api_secret,
+            'Accept: application/json'));
+        curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($handle);
+        $cError = curl_error( $handle );
 
-              if( $cError )
-              {
-                     // Log this error.
-                     $this->log->error('apiCall error', array(
-                            'message' => $cError,
-                            'url' => $full_url
-                            ) );
-              }
-              
-                     
-              curl_close($handle);
-              return $result;
-       }
+        if( $cError )
+        {
+                // Log this error.
+                $this->log->error('apiCall error', array(
+                    'message' => $cError,
+                    'url' => $full_url
+                    ) );
+        }
+        
+        curl_close($handle);
+        return $result;
+    }
 
-       public function getDomainList($api_key, $api_secret)
-       {
-              $domain_list = array();
-              $domain_count = 0;
+    public function getDomainList($api_key, $api_secret)
+    {
+        $domain_list = array();
+        $domain_count = 0;
 
-              $api_url = $this->getApiUrl('', 'domainlist');
-              $api_results = $this->apiCall($api_key, $api_secret, $api_url);
-              $array_results = $this->convertToArray($api_results);
+        $api_url = $this->getApiUrl('', 'domainlist');
+        $api_results = $this->apiCall($api_key, $api_secret, $api_url);
+        $array_results = $this->convertToArray($api_results);
 
-              // confirm that the api call was successful
-              if (isset($array_results[0]['domain'])) {
+        // confirm that the api call was successful
+        if (isset($array_results[0]['domain'])) {
 
-                     foreach ($array_results as $domain) {
+            foreach ($array_results as $domain) {
 
-                            $domain_list[] = $domain['domain'];
-                            $domain_count++;
-                            
-                     }
+                $domain_list[] = $domain['domain'];
+                $domain_count++;
 
-              } else {
+            }
 
-                     $log_message = 'Unable to get domain list: '.$api_results;
-                     $log_extra = array('API Key' => $this->format->obfusc($api_key), 'API Secret' => $this->format->obfusc($api_secret));
-                     $this->log->error($log_message, $log_extra);
+        } else {
 
-              }
+            $log_message = 'Unable to get domain list: '.$api_results;
+            $log_extra = array('API Key' => $this->format->obfusc($api_key), 'API Secret' => $this->format->obfusc($api_secret));
+            $this->log->error($log_message, $log_extra);
 
-              return array($domain_count, $domain_list);
-       }
+        }
 
-       public function getFullInfo($api_key, $api_secret, $domain)
-       {
-              $expiration_date = '';
-              $dns_servers = array();
-              $privacy_status = '';
-              $autorenewal_status = '';
+        return array($domain_count, $domain_list);
+    }
 
-              $api_url = $this->getApiUrl($domain, 'info');
-              $api_results = $this->apiCall($api_key, $api_secret, $api_url);
-              $array_results = $this->convertToArray($api_results);
+    public function getFullInfo($api_key, $api_secret, $domain)
+    {
+        $expiration_date = '';
+        $dns_servers = array();
+        $privacy_status = '';
+        $autorenewal_status = '';
 
-              // confirm that the api call was successful
-              if (isset($array_results['domain'])) {
+        $api_url = $this->getApiUrl($domain, 'info');
+        $api_results = $this->apiCall($api_key, $api_secret, $api_url);
+        $array_results = $this->convertToArray($api_results);
 
-                     // get expiration date
-                     $expiration_date = substr($array_results['expires'], 0, 10);
+        // confirm that the api call was successful
+        if (isset($array_results['domain'])) {
 
-                     // get dns servers
-                     $dns_result = $array_results['nameServers'];
-                     $dns_servers = $this->processDns($dns_result);
+            // get expiration date
+            $expiration_date = substr($array_results['expires'], 0, 10);
 
-                     // get privacy status
-                     $privacy_result = (string) $array_results['privacy'];
-                     $privacy_status = $this->processPrivacy($privacy_result);
+            // get dns servers
+            $dns_result = $array_results['nameServers'];
+            $dns_servers = $this->processDns($dns_result);
 
-                     // get auto renewal status
-                     $autorenewal_result = (string) $array_results['renewAuto'];
-                     $autorenewal_status = $this->processAutorenew($autorenewal_result);
+            // get privacy status
+            $privacy_result = (string) $array_results['privacy'];
+            $privacy_status = $this->processPrivacy($privacy_result);
 
-              } else {
+            // get auto renewal status
+            $autorenewal_result = (string) $array_results['renewAuto'];
+            $autorenewal_status = $this->processAutorenew($autorenewal_result);
 
-                     $log_message = 'Unable to get domain details';
-                     $log_extra = array('Domain' => $domain, 'API Key' => $this->format->obfusc($api_key), 'API Secret' => $this->format->obfusc($api_secret));
-                     $this->log->error($log_message, $log_extra);
+        } else {
 
-              }
+            $log_message = 'Unable to get domain details';
+            $log_extra = array('Domain' => $domain, 'API Key' => $this->format->obfusc($api_key), 'API Secret' => $this->format->obfusc($api_secret));
+            $this->log->error($log_message, $log_extra);
 
-              return array($expiration_date, $dns_servers, $privacy_status, $autorenewal_status);
-       }
+        }
 
-       public function convertToArray($api_result)
-       {
-              return json_decode($api_result, true);
-       }
+        return array($expiration_date, $dns_servers, $privacy_status, $autorenewal_status);
+    }
 
-       public function processDns($dns_result)
-       {
-              $dns_servers = array();
-              if (!empty($dns_result)) {
-                     $dns_servers = array_filter($dns_result);
-              } else {
-                     $dns_servers[0] = 'no.dns-servers.1';
-                     $dns_servers[1] = 'no.dns-servers.2';
-              }
-              return $dns_servers;
-       }
+    public function convertToArray($api_result)
+    {
+        return json_decode($api_result, true);
+    }
 
-       public function processPrivacy($privacy_result)
-       {
-              if ($privacy_result == '') {
-                     $privacy_status = '0';
-              } else {
-                     $privacy_status = '1';
-              }
-              return $privacy_status;
-       }
+    public function processDns($dns_result)
+    {
+        $dns_servers = array();
+        if (!empty($dns_result)) {
+            $dns_servers = array_filter($dns_result);
+        } else {
+            $dns_servers[0] = 'no.dns-servers.1';
+            $dns_servers[1] = 'no.dns-servers.2';
+        }
+        return $dns_servers;
+    }
 
-       public function processAutorenew($autorenewal_result)
-       {
-              if ($autorenewal_result == '') {
-                     $autorenewal_status = '0';
-              } else {
-                     $autorenewal_status = '1';
-              }
-              return $autorenewal_status;
-       }
+    public function processPrivacy($privacy_result)
+    {
+        if ($privacy_result == '') {
+            $privacy_status = '0';
+        } else {
+            $privacy_status = '1';
+        }
+        return $privacy_status;
+    }
+
+    public function processAutorenew($autorenewal_result)
+    {
+        if ($autorenewal_result == '') {
+            $autorenewal_status = '0';
+        } else {
+            $autorenewal_status = '1';
+        }
+        return $autorenewal_status;
+    }
 
 } //@formatter:on
