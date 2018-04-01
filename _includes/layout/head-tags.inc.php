9c14e12c161b6b2af61a8ad14c5a1f0e48501dca
Greg Chetcuti <greg@chetcuti.com>
2018-03-31 20:59:50 -0700

    Remove unnecessaryy URI schemes

--- _includes/layout/head-tags.inc.php
+++ _includes/layout/head-tags.inc.php
@@ -36,9 +36,9 @@
 <!-- Bootstrap 3.3.5 -->
 <link rel="stylesheet" href="<?php echo $web_root . '/' . WEBROOT_THEME; ?>/bootstrap/css/bootstrap.min.css">
 <!-- Font Awesome -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <!-- Ionicons -->
-<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<link rel="stylesheet" href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 <!-- DataTables -->
 <?php /* ?>
 <!link rel="stylesheet" href="<?php echo $web_root . '/' . WEBROOT_THEME; ?>/plugins/datatables/jquery.dataTables.css">
@@ -58,6 +58,6 @@
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<script src="//oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
