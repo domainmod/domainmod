37c9d34829d2b9f7b5242ec3475c09b21f50500b
Greg Chetcuti <greg@chetcuti.com>
2019-01-03 22:52:36 -0800

    Update copyright

--- assets/add/dns.php
+++ assets/add/dns.php
@@ -3,7 +3,7 @@
  * /assets/add/dns.php
  *
  * This file is part of DomainMOD, an open source domain and internet asset manager.
- * Copyright (c) 2010-2018 Greg Chetcuti <greg@chetcuti.com>
+ * Copyright (c) 2010-2019 Greg Chetcuti <greg@chetcuti.com>
  *
  * Project: http://domainmod.org   Author: http://chetcuti.com
  *
