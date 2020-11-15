00dc92b254c885aa77a79ae0b525b7cbfa02b17e
Greg Chetcuti <greg@chetcuti.com>
2020-11-15 10:03:22 -0800

    Add check to language detection

--- classes/DomainMOD/Goal.php
+++ classes/DomainMOD/Goal.php
@@ -62,7 +62,7 @@ class Goal
         $act_new_version = SOFTWARE_VERSION;
         $act_ip_address = $this->getIp();
         $act_agent = $_SERVER['HTTP_USER_AGENT'];
-        $act_language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+        $act_language = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : 'en';
 
         $stmt = $pdo->prepare("
             INSERT INTO goal_activity
