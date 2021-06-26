66de6f0800d27a9c44c7e3f8aa8b5b2e64402816
Greg Chetcuti <greg@chetcuti.com>
2021-06-25 23:46:22 -0700

    Add support for head-tags.DEMO.inc.php

--- _includes/layout/head-tags.inc.php
+++ _includes/layout/head-tags.inc.php
@@ -61,3 +61,11 @@
 <script src="//oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
+<?php
+$full_filename = DIR_INC . '/layout/head-tags.DEMO.inc.php';
+
+if (file_exists($full_filename)) {
+
+    require_once DIR_INC . '/layout/head-tags.DEMO.inc.php';
+
+}
