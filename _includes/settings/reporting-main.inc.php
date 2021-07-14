332e16085767221de959697fa51b446345e46662
Greg Chetcuti <greg@chetcuti.com>
2021-07-13 17:21:36 -0700

    Update Main Reporting section settings

--- _includes/settings/reporting-main.inc.php
+++ _includes/settings/reporting-main.inc.php
@@ -25,35 +25,3 @@ $breadcrumb = $page_title;
 $software_section = "reporting";
 $software_section_logo = "fa-bar-chart";
 $slug = "reporting-main";
-$datatable_class = 'table table-striped dt-responsive cell-border compact';
-$datatable_options = 'var oldStart = 0;
-                      $(\'#' . $slug . '\').DataTable({
-                          "paging": true,
-                          "lengthChange": true,
-                          "lengthMenu": [ [25, 50, 75, 100, -1], [25, 50, 75, 100, "All"] ],
-                          "searching": true,
-                          "info": true,
-                          "autoWidth": true,
-                          "bAutoWidth": false,
-                          "responsive": {
-                               details: {
-                                         type: "column"
-                                        }
-                                        },
-                          "columnDefs": [ {
-                                           className: "control",
-                                           orderable: false,
-                                           targets:   0
-                                           } ],
-                          "ordering": true,
-                          "order": [[ 1, "asc" ]],
-                          "bSortClasses": false,
-                          "dom": \'<"top"lif>rt<"bottom"ip><"clear">\',
-                          "fnDrawCallback": function (o) {
-                            if ( o._iDisplayStart != oldStart ) {
-                                var targetOffset = $("#' . $slug . '").offset().top;
-                                $("html,body").animate({scrollTop: targetOffset}, 0);
-                                oldStart = o._iDisplayStart;
-                            }
-                          }
-                      });';
