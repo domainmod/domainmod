2b76e5faeffbcd2893b875252ef63717451d335d
Greg Chetcuti <greg@chetcuti.com>
2023-09-04 02:14:17 -0700

    Database Update v4.20.01

--- _includes/software.inc.php
+++ _includes/software.inc.php
@@ -21,7 +21,7 @@
 ?>
 <?php
 const SOFTWARE_TITLE = 'DomainMOD';
-const SOFTWARE_VERSION = '4.20.0';
+const SOFTWARE_VERSION = '4.20.01';
 define('WEB_ROOT', $web_root);
 define('DB_HOSTNAME', $dbhostname);
 define('DB_NAME', $dbname);
--- _includes/updates/4.06.00-current.inc.php
+++ _includes/updates/4.06.00-current.inc.php
@@ -1519,6 +1519,31 @@ if ($current_db_version === '4.19.0') {
 
         $pdo->beginTransaction();
 
+        $upgrade->database($new_version);
+
+        if ($pdo->InTransaction()) $pdo->commit();
+        $current_db_version = $new_version;
+
+    } catch (Exception $e) {
+
+        if ($pdo->InTransaction()) $pdo->rollback();
+        $upgrade->logFailedUpgrade($old_version, $new_version, $e);
+        throw $e;
+
+    }
+
+} //@formatter:on
+
+// upgrade database from 4.20.0 to 4.20.01
+if ($current_db_version === '4.20.0') {
+
+    $old_version = '4.20.0';
+    $new_version = '4.20.01';
+
+    try {
+
+        $pdo->beginTransaction();
+
         /*
          * This needs to be MOVED from the last version to the newest version with every release
          */
