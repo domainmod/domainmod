75a7ab8a7d02c85f6a94ec13e1529aaedb646d8b
Greg Chetcuti <greg@chetcuti.com>
2023-09-04 20:06:56 -0700

    Database Update v4.20.03

--- _includes/updates/4.06.00-current.inc.php
+++ _includes/updates/4.06.00-current.inc.php
@@ -1590,6 +1590,33 @@ if ($current_db_version === '4.20.01') {
 
         }
 
+        $goal->upgrade($previous_version);
+
+        $upgrade->database($new_version);
+
+        if ($pdo->InTransaction()) $pdo->commit();
+        $current_db_version = $new_version;
+
+    } catch (Exception $e) {
+
+        if ($pdo->InTransaction()) $pdo->rollback();
+        $upgrade->logFailedUpgrade($old_version, $new_version, $e);
+        throw $e;
+
+    }
+
+} //@formatter:on
+
+// upgrade database from 4.20.02 to 4.20.03
+if ($current_db_version === '4.20.02') {
+
+    $old_version = '4.20.02';
+    $new_version = '4.20.03';
+
+    try {
+
+        $pdo->beginTransaction();
+
         /*
          * This needs to be MOVED from the last version to the newest version with every release
          */
